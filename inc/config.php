785ba10ce7c4adb8934479f01fa2d339f9c51363
Maurice Henke <maurice.henke.bo@gmail.com>
2019-01-20 20:04:16 +0100

    Delete config.php

--- inc/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DATABASE_HOST', 'localhost');			// MySQL host. usually localhost
-define('DATABASE_USER', 'tp');					// MySQL username
-define('DATABASE_PASS', 'w2FxhE56hELBnfAFCG2L');// MySQL password
-define('DATABASE_NAME', 'tp_routes');		    // Database name
-define('DATABASE_WHAT', 'host');	    	    // "host" or unix socket path
